0c9737f66d910c5c64a99e116c61b5366d99d10b
manisha goyal <48145474+mahigoyal123@users.noreply.github.com>
2021-05-26 19:29:05 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,39 @@
-import React, {Component, useState} from "react";
+import React, { useState} from "react";
+//import { slides } from './data'
 import '../styles/App.css';
 
 const App = () => {
+
+  const [currSlides, setCurrSlides] = useState(0)
+
+  const clickPrev=()=>{
+  
+       currSlides >0 && setCurrSlides(currSlides-1);
+       }
+  
+  
+  const clickNext=()=>{
+       currSlides <slides.length-1 && setCurrSlides(currSlides+1);
+       }
+  
+  const clickRestart = () => {
+      setCurrSlides(0);
+  }     
+
+
   return (
-    <></>
+    <>
+     <div className="App">
+      <div className="carousel">
+            <h1 data-testid="title">{slides[currSlides].title}</h1>
+            <p data-testid="text">{slides[currSlides].text}</p>
+        </div>
+        <button onClick={clickPrev} disabled={currSlides===0?true:false} data-testid="button-restart" >Prev</button>
+        <button onClick={clickNext} disabled={currSlides===slides.length-1?true:false} data-testid="button-prev">Next</button>
+        <button onClick={clickRestart} disabled={currSlides===0?true:false} data-testid="button-next">Restart</button>
+    </div> 
+    
+    </>
   )
 }
 
